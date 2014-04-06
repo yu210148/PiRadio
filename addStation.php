3310e9be9c37ab6b14483e1d551d623a2678d71c
Kevin Lucas <yu210148@gmail.com>
2014-04-06 10:00:39 -0400

    Added licence info

--- addStation.php
+++ addStation.php
@@ -13,6 +13,25 @@
 </head>
 <body>
 <?php
+/*
+    PiRadio Plays an assortment of radio stations on a webhost (I've got a
+    Raspberry Pi on my bookshelf with a pair of speakers plugged into it.)
+    
+    Copyright (C) 2014  Kevin Lucas
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 require_once 'settings.php';
 
