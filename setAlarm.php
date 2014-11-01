b43ec8e2b4dae6315d0bd817b184c36c7d483531
Kevin Lucas <yu210148@gmail.com>
2014-11-01 23:35:49 +0000

    need to query db before looking at results

--- setAlarm.php
+++ setAlarm.php
@@ -97,6 +97,7 @@ function set_alarm($db, $stationName, $date, $time){
 
     // get station ID
     $sql = "SELECT stations.StationURL FROM stations WHERE stations.Name = '$stationName'";
+	 $q = mysqli_query($db, $sql);
     while ($row = mysqli_fetch_array($q, MYSQLI_NUM)){
       $stationURL = $row[0];
     } // end while
