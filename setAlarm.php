4e118caa00cc0d59d02362495c0661af87aa08a3
Kevin Lucas <yu210148@gmail.com>
2014-11-01 23:50:53 +0000

    added in additional debugging code

--- setAlarm.php
+++ setAlarm.php
@@ -97,6 +97,8 @@ function set_alarm($db, $stationName, $date, $time){
 
     // get station ID
     $sql = "SELECT stations.StationURL FROM stations WHERE stations.Name = '$stationName'";
+  //debug
+  var_dump($sql);
   
 	 $q = mysqli_query($db, $sql);
     while ($row = mysqli_fetch_array($q, MYSQLI_NUM)){
