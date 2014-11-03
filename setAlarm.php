11b5eb0150054895e0bf648f73713eac28f39627
Kevin Lucas <klucas@uoftbookstore.com>
2014-11-03 16:46:35 -0500

    need the output lines in an array

--- setAlarm.php
+++ setAlarm.php
@@ -136,7 +136,7 @@ function show_set_alarms(){
     // a function to retreive alamrs already set and display them on the screen
     $command = "atq";
     $command = escapeshellcmd($command);
-    $output = exec($command);
+    $output[] = exec($command);
     
     //debug
     var_dump($output);
