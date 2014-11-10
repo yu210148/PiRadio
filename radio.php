1d400c65a92c9494524cc829fb1c481c2b949be4
Kevin Lucas <klucas@uoftbookstore.com>
2014-11-10 16:25:42 -0500

    Added a TODO note for an update feature
    Added a todo note to remind me to add a way
    to update the PiRadio software through the
    UI.

--- radio.php
+++ radio.php
@@ -33,6 +33,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+//TODO: Add a button to update the PiRadio software
+// it should just be a matter of having the button
+// run git pull if the directory permissions are set right.
 
 require_once 'settings.php';
 
