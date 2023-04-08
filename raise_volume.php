9f6f7285031ead0109569cf130ad3276b3ddd808
Kevin Lucas <yu210148@gmail.com>
2023-04-08 11:27:20 -0400

    Update raise_volume.php
    specify the sound card interface to raise or lower

--- raise_volume.php
+++ raise_volume.php
@@ -1,6 +1,6 @@
 <?php
 function raise_volume(){
-    $command = "amixer set PCM 2dB+";
+    $command = "amixer -c 1 set PCM 2dB+";
     $command = escapeshellcmd($command);
     exec($command);
     return 0;
